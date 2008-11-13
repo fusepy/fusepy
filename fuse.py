870de4e94a5a574b1df84cbb523fb4fcc59a1477
verigak <verigak@1ebed218-b0a3-11dd-8075-91d349ce83ee>
2008-11-13 17:38:56 +0000

    Updated the email of the copyright.
    git-svn-id: http://fusepy.googlecode.com/svn/trunk@3 1ebed218-b0a3-11dd-8075-91d349ce83ee

--- fuse.py
+++ fuse.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2008 Giorgos Verigakis <verigak@cslab.ece.ntua.gr>
+# Copyright (c) 2008 Giorgos Verigakis <verigak@gmail.com>
 # 
 # Permission to use, copy, modify, and distribute this software for any
 # purpose with or without fee is hereby granted, provided that the above
