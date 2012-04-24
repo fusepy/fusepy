2fcefe9eba09594c221ce4dceace55791afac192
Terence Honles <terence@honles.com>
2012-04-24 02:42:03 -0700

    removing fuse3x which can be generated by 2to3

--- fuse/__init__.py
+++
@@ -1,6 +0,0 @@
-__version__ = '1.2'
-
-try:
-    from fuse3x import *
-except ImportError:
-    from fuse2x import *
--- setup.py
+++ setup.py
@@ -1,17 +1,28 @@
 #!/usr/bin/env python
 
-from setuptools import setup, find_packages
+from setuptools import setup
 
-from fuse import __version__
+try:
+    from lib2to3 import refactor
+    fixers = set(refactor.get_fixers_from_package('lib2to3.fixes'))
+except ImportError:
+    fixers = set()
 
 setup(
     name = 'fusepy',
-    version = __version__,
-    packages = find_packages(),
+    version = '1.2',
 
     description = 'Simple ctypes bindings for FUSE',
     author = 'Giorgos Verigakis',
     author_email = 'verigak@gmail.com',
-    url = 'http://code.google.com/p/fusepy/',
     license = 'ISC',
+    py_modules=['fuse'],
+
+    use_2to3 = True,
+    # only use the following fixers (everything else is already compatible)
+    use_2to3_exclude_fixers = fixers - set([
+        'lib2to3.fixes.fix_except',
+        'lib2to3.fixes.fix_future',
+        'lib2to3.fixes.fix_numliterals',
+    ]),
 )
