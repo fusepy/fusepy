b4bf436864eb54148106233f5c71cb1e1d9fa9f8
kzwin <kzw@happyw.info>
2013-03-06 03:10:30 -0500

    fix hard link

--- examples/loopback.py
+++ examples/loopback.py
@@ -44,7 +44,7 @@ class Loopback(LoggingMixIn, Operations):
     getxattr = None
 
     def link(self, target, source):
-        return os.link(source, target)
+        return os.link( self.root + source, target)
 
     listxattr = None
     mkdir = os.mkdir
