e1dd73ac2f11bcbc6717ae3fc0e8e501ce12f081
Rian Hunter <rian@alum.mit.edu>
2016-03-31 01:21:59 -0700

    Ignore input time field if it doesn't exist on stat

--- fuse.py
+++ fuse.py
@@ -383,7 +383,9 @@ def time_of_timespec(ts):
 def set_st_attrs(st, attrs):
     for key, val in attrs.items():
         if key in ('st_atime', 'st_mtime', 'st_ctime', 'st_birthtime'):
-            timespec = getattr(st, key + 'spec')
+            timespec = getattr(st, key + 'spec', None)
+            if timespec is None:
+                continue
             timespec.tv_sec = int(val)
             timespec.tv_nsec = int((val - timespec.tv_sec) * 10 ** 9)
         elif hasattr(st, key):
