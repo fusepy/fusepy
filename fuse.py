e22539297c87cd3659b7f5ccf0341a47c1c106bd
pjhades <pj.hades@gmail.com>
2016-06-22 19:24:59 -0300

    Fix: getxattr throws TypeError

--- fuse.py
+++ fuse.py
@@ -644,7 +644,8 @@ class FUSE(object):
 
     def getxattr(self, path, name, value, size, *args):
         ret = self.operations('getxattr', path.decode(self.encoding),
-                                          name.decode(self.encoding), *args)
+                                          name.decode(self.encoding), *args) \
+                  .encode(self.encoding)
 
         retsize = len(ret)
         # allow size queries
